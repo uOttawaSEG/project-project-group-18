f85a4de4a5d56358828e5ee605ce113bf92c0ee7
keno <keno.obuwoma@gmail.com>
2024-10-06 22:33:44 -0400

    added draft design for login page

---
+++ Welcomescreen/app/src/main/java/com/example/welcomescreen/MainActivity.java
@@ -0,0 +1,66 @@
+package com.example.welcomescreen;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    EditText username;
+    EditText password;
+    Button loginOrganizer;
+    Button login;
+    Button signup;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        loginOrganizer = findViewById(R.id.loginOrganizer);
+        login = findViewById(R.id.login);
+        signup = findViewById(R.id.signup);
+
+        loginOrganizer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(username.getText().toString().equals("user")&& password.getText().toString().equals("1234")){
+                    Toast.makeText(MainActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Toast.makeText(MainActivity.this, "Login Failed!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        login.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                if(username.getText().toString().equals("user")&& password.getText().toString().equals("1234")){
+                    Toast.makeText(MainActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Toast.makeText(MainActivity.this, "Login Failed!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+        }
+    }

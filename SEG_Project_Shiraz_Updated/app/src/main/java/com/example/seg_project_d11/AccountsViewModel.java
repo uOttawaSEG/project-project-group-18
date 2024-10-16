308f66ecd8cba37e9e68099c6ce2c1b89e291081
Shiraz <shatamna@gmail.com>
2024-10-16 17:49:20 -0400

    Minor changes

--- SEG_Project_Shiraz_Updated/app/src/main/java/com/example/seg_project_d11/AccountsViewModel.java
+++ SEG_Project_Shiraz_Updated/app/src/main/java/com/example/seg_project_d11/AccountsViewModel.java
@@ -1,8 +1,7 @@
 package com.example.seg_project_d11;
 import androidx.lifecycle.ViewModel;
 
-public class OrganizationViewModel extends ViewModel{
+public class AccountsViewModel extends ViewModel{
     public static String organizerName, organizerPhone, organizerLastName, organizerAddress, organizationName, organizerEmail, organizerPassword = null;
     public static String attendeeName, attendeePhone, attendeeLastName, attendeeAddress, attendeeEmail, attendeePassword = null;
-
 }

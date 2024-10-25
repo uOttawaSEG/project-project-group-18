dd82c1ef859925d5ccd2d1fe7c42e29e193c93cf
MistyHaven <mistyhaven150@gmail.com>
2024-10-25 13:45:14 -0400

    -> lil edits

--- SEG_Project_Shiraz_Updated/app/src/main/java/com/example/seg_project_d11/SelectionActivity.java
+++ SEG_Project_Shiraz_Updated/app/src/main/java/com/example/seg_project_d11/SelectionActivity.java
@@ -1,6 +1,8 @@
 package com.example.seg_project_d11;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
@@ -10,7 +12,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class SelectionActivity extends AppCompatActivity {
-    Button signAtendee;
+    Button signAttendee;
     Button signOrganizer;
     Button signAdmin;
 
@@ -24,9 +26,36 @@ public class SelectionActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        signAtendee = findViewById(R.id.buttonAttendee);
+        signAttendee = findViewById(R.id.buttonAttendee);
         signOrganizer = findViewById(R.id.buttonOrganizer);
         signAdmin = findViewById(R.id.buttonAdministrator);
+
+        signAttendee.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //goes back to Organizer_1 activity
+                Intent intent = new Intent(SelectionActivity.this, SignInPage.class);
+                startActivity(intent);
+            }
+        });
+
+        signOrganizer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //goes back to Organizer_1 activity
+                Intent intent = new Intent(SelectionActivity.this, SignInPage.class);
+                startActivity(intent);
+            }
+        });
+
+        signAdmin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //goes back to Organizer_1 activity
+
+            }
+        });
+
     }
 
 }
